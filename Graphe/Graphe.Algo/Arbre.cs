6165261d2fd8830a9a0cbbbeb66616cad2784c21
RASOLOFONIAINA Mickael <mickael.rasolofoniaina@proximitybbdo.fr>
2019-03-21 21:44:45 +0300

    add tree structure

---
+++ Graphe/Graphe.Algo/Arbre.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Graphe.Algo
+{
+    class Arbre<T>
+    {
+        public T Noeud { get; set; }
+        public List<Arbre<T>> Fils { get; set; }
+    }
+}
