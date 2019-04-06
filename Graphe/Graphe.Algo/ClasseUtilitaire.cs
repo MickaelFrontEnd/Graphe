1dc7d7d16915fc21b80e414d918330c9cba155bc
RASOLOFONIAINA Mickael <mickael.rasolofoniaina@proximitybbdo.fr>
2019-04-06 18:03:07 +0300

    add class utilities

---
+++ Graphe/Graphe.Algo/ClasseUtilitaire.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace Graphe
+{
+    // Base classe avec noeud
+    public class BaseNoeud<T>
+    {
+        public T Noeud { get; set; }
+    }
+    
+    // Noeud + Degres
+    public class NoeudDegre<T> : BaseNoeud<T>
+    {
+        public int Degres { get; set; }
+    }
+
+    // Noeud + Couleur
+    public class NoeudCouleur<T> : BaseNoeud<T>
+    {
+        public string Couleur { get; set; }
+    }
+}
---
+++ Graphe/Graphe.Algo/FonctionUtilitaire.cs
@@ -0,0 +1,15 @@
+﻿
+
+using System;
+
+namespace Graphe
+{
+    public class FonctionUtilitaire
+    {
+        public static string GenererCouleur()
+        {
+            var random = new Random();
+            return String.Format("#{0:X6}", random.Next(0x1000000));
+        }
+    }
+}
